6e12500c8ea799d548f5d1333504b4e6f1b1de12
Avaab Razzaq <40202832+AR10Dev@users.noreply.github.com>
2022-10-03 11:33:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,7 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-import { tauri } from 'vite-plugin-tauri'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    solidPlugin(),
-    tauri()
-  ],
-  // prevent vite from obscuring rust errors
-  clearScreen: false,
-  // Tauri expects a fixed port, fail if that port is not available
-  server: {
-    strictPort: true,
-    open: false
-  },
-  // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
-  // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
-  // env variables
-  envPrefix: ['VITE_', 'TAURI_'],
-  build: {
-    // Tauri supports es2021
-    target: ['es2022', 'chrome100', 'safari13'],
-    // don't minify for debug builds
-    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
-    // produce sourcemaps for debug builds
-    sourcemap: !!process.env.TAURI_DEBUG,
-  }
+  plugins: [solidPlugin()]
 })
