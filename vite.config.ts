03a03c7968605eb8468a3c1cc7b1c2cbaa59f0d1
Avaab Razzaq <40202832+AR10Dev@users.noreply.github.com>
2022-10-03 11:22:25 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import { tauri } from 'vite-plugin-tauri'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    tauri()
+  ],
   // prevent vite from obscuring rust errors
   clearScreen: false,
   // Tauri expects a fixed port, fail if that port is not available
   server: {
     strictPort: true,
+    open: false
   },
   // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
   // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
