e2c29d749d9a538696c11dbc89c87007456dd7bf
Avaab Razzaq <40202832+AR10Dev@users.noreply.github.com>
2023-12-10 13:46:10 +0000

    Add @ianvs/prettier-plugin-sort-imports plugin and its options

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,13 @@
+/** @type {import("@ianvs/prettier-plugin-sort-imports").PrettierConfig} */
 module.exports = {
   arrowParens: 'avoid',
   singleQuote: true,
   printWidth: 90,
   semi: false,
   trailingComma: 'none',
-  plugins: ['prettier-plugin-tailwindcss'],
+  plugins: ['prettier-plugin-tailwindcss','@ianvs/prettier-plugin-sort-imports'],
+  // @ianvs/prettier-plugin-sort-imports plugin's options
+  // https://github.com/IanVS/prettier-plugin-sort-imports#options
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.3.3',
 }
