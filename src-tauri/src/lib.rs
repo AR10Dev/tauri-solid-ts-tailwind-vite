3e7b545ce7a6dff54b07e8a6af27e4624828a53d
Avaab Razzaq <40202832+AR10Dev@users.noreply.github.com>
2025-02-25 13:32:38 +0100

    📦 chore(deps): Add tauri-plugin-devtools v2.0.0 and enable in development builds (#129)

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,16 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
+  #[cfg(debug_assertions)] // only enable instrumentation in development builds
+  let devtools = tauri_plugin_devtools::init();
+
+  let mut builder = tauri::Builder::default();
+
+  #[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(devtools);
+    }
+
+  builder
     // .plugin( /* Add your Tauri plugin here */ )
     // Add your commands here that you will call from your JS code  
     // .invoke_handler(tauri::generate_handler![ /* Add your commands here */ ])
