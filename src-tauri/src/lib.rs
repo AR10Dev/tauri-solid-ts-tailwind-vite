64fb09b60911fffe151a4b3ca60061c254e92fda
Avaab Razzaq <40202832+AR10Dev@users.noreply.github.com>
2025-03-07 20:09:58 +0000

    chore: update Rust edition to 2024

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,19 +1,19 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  #[cfg(debug_assertions)] // only enable instrumentation in development builds
-  let devtools = tauri_plugin_devtools::init();
+    #[cfg(debug_assertions)] // only enable instrumentation in development builds
+    let devtools = tauri_plugin_devtools::init();
 
-  let mut builder = tauri::Builder::default();
+    let mut builder = tauri::Builder::default();
 
-  #[cfg(debug_assertions)]
+    #[cfg(debug_assertions)]
     {
         builder = builder.plugin(devtools);
     }
 
-  builder
-    // .plugin( /* Add your Tauri plugin here */ )
-    // Add your commands here that you will call from your JS code  
-    // .invoke_handler(tauri::generate_handler![ /* Add your commands here */ ])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    builder
+        // .plugin( /* Add your Tauri plugin here */ )
+        // Add your commands here that you will call from your JS code
+        // .invoke_handler(tauri::generate_handler![ /* Add your commands here */ ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
