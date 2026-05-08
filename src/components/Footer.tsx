6df93ac44211ee2d30a863a0f7f7e7c118d651fc
Avaab Razzaq <40202832+AR10Dev@users.noreply.github.com>
2026-05-08 04:59:33 +0000

    feat: Update footer content to include author credit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,10 @@
 import type { Component } from 'solid-js'
+import Link from './Link'
 
 const Footer: Component = () => {
   return (
-    <footer class="mt-8 text-gray-600">
-      &copy; {new Date().getFullYear()} My App. All rights reserved.
+    <footer class="mt-8 text-lg font-medium text-gray-600">
+      Made with ❤️ by <Link href="https://avaabrazzaq.com">Avaab Razzaq</Link>
     </footer>
   )
 }
